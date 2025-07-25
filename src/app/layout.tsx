ce419962865a2683374b350151ee206caf5dfd76
Aatif Ahmad <aatifahmad0710@gmail.com>
2025-07-26 02:08:14 +0530

    Update Google Tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,18 +15,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
-        {/* Google Analytics scripts */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PFM57SNCYV"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-PFM57SNCYV');
-            `,
-          }}
-        />
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LTSPT7X0V9"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-LTSPT7X0V9');
+        </script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
